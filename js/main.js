5387ee54190988d8991f0ed102678f66e75147f7
Sakil Anwar <sakilanwar60@gmail.com>
2023-01-19 23:58:40 +0600

    project setup

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+
+// header
+
+const openMenuBtn = document.querySelector('.header-area .open-menu-btn button');
+const mobileMenu = document.querySelector('.header-area .mobile-menu-area');
+const closeBtn = document.querySelector('.header-area .close-btn button');
+const body = document.querySelector('body');
+
+openMenuBtn.addEventListener('click', function () {
+    mobileMenu.classList.add('toggle-mobile-menu');
+    body.classList.add('bg-gray');
+})
+closeBtn.addEventListener('click', function () {
+    mobileMenu.classList.remove('toggle-mobile-menu');
+    body.classList.remove('bg-gray');
+})
+
+
+
+
+
+// preloader
+
+// window.addEventListener('load', function () {
+//     document.querySelector('#loader-wrapper').classList.add("loaded")
+// });
+
