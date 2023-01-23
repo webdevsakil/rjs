8f6254f5bf4716e87cd79a9526d719657a43e706
Sakil Anwar <sakilanwar60@gmail.com>
2023-01-23 21:59:24 +0600

    video added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 
-if (document.getElementById('video') !== null) {
-    const video = document.getElementById('video');
-    const videoPlayer = document.querySelector('#vedioWrapper img');
+// if (document.getElementById('video') !== null) {
+//     const video = document.getElementById('video');
+//     const videoPlayer = document.querySelector('#vedioWrapper img');
 
-    videoPlayer.addEventListener('click', function () {
-        video.play();
-    })
+//     videoPlayer.addEventListener('click', function () {
+//         video.play();
+//     })
 
-    video.onplaying = function () {
-        videoPlayer.style.display = 'none';
-    };
+//     video.onplaying = function () {
+//         videoPlayer.style.display = 'none';
+//     };
 
-    video.addEventListener('pause', function () {
-        videoPlayer.style.display = 'block';
-    })
+//     video.addEventListener('pause', function () {
+//         videoPlayer.style.display = 'block';
+//     })
 
-}
+// }
 $(document).ready(function () {
-
+    
     $(".open-menu-btn").on('click', function () {
         $('.mobile-menu-area').addClass('toggle-mobile-menu')
     })
@@ -35,6 +35,42 @@ $(document).ready(function () {
         speed: 300,
         prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-left-long'></i></button>",
         nextArrow: "<button type='button' class='slick-next pull-right'> <i class='fa-solid fa-arrow-right-long'></i> </button>",
+    });
+
+    // popup video
+
+    $('.video-player').magnificPopup({
+        type: 'iframe',
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' +
+                '<div class="mfp-close"></div>' +
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+                    id: 'v=', 
+                   
+                    src: 'https://www.youtube.com/embed/%id%?autoplay=1' 
+                },
+                vimeo: {
+                    index: 'vimeo.com/',
+                    id: '/',
+                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                },
+                gmaps: {
+                    index: '//maps.google.',
+                    src: '%id%&output=embed'
+                }
+
+
+            },
+
+            srcAction: 'iframe_src', 
+        }
+        // other options
     });
 
 });
